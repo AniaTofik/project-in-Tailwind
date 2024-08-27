a5bafe7a11bc835bc2ff81057f9bdebc1086a344
AniaTofik <anna.kiedel@onet.pl>
2024-08-27 17:37:56 +0200

    deleted node modules file

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const btnSubscribe = document.querySelector(".btn-subscribe");
 const btnDismiss = document.querySelector(".btn-dismiss");
 const subscribeCard = document.querySelector(".subscribe-card");
 const dismissCard = document.querySelector(".dismiss-card");
+const emailPopup = document.querySelector(".emailPopup");
+const emailInput = document.getElementById("emailInput");
 
 const openDismissCard = () => {
   subscribeCard.classList.add("hidden");
@@ -11,7 +13,24 @@ const openDismissCard = () => {
 const closeDismissCard = () => {
   dismissCard.classList.add("hidden");
   subscribeCard.classList.remove("hidden");
+  emailInput.value = "";
+  btnSubscribe.disabled = true;
 };
+
+function validateEmailInput() {
+  const emailInput = document.getElementById("emailInput").value;
+  const feedbackElement = document.getElementById("emailFeedback");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(emailInput)) {
+    feedbackElement.textContent = "";
+    emailPopup.textContent = emailInput;
+    btnSubscribe.disabled = false;
+  } else {
+    feedbackElement.textContent = "Valid email required";
+    btnSubscribe.disabled = true;
+  }
+}
 
 btnSubscribe.addEventListener("click", () => {
   subscribeCard.classList.remove("lg:flex");
@@ -23,3 +42,4 @@ btnDismiss.addEventListener("click", () => {
 
 btnSubscribe.addEventListener("click", openDismissCard);
 btnDismiss.addEventListener("click", closeDismissCard);
+emailInput.addEventListener("input", validateEmailInput);
